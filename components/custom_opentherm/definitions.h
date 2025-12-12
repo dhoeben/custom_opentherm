be26cc1043b7870b8d1be7713bcc3ed45cb53316
dhoeben <douwehoeben@gmail.com>
2025-12-12 15:13:18 +0100

    .

--- components/custom_opentherm/definitions.h
+++ components/custom_opentherm/definitions.h
@@ -1,11 +1,25 @@
 #pragma once
 #include <cstdint>
 
+// Message Types (Read/Write)
+enum OpenThermMessageType : uint8_t {
+    OT_MSG_READ_DATA   = 0,    // 0x00
+    OT_MSG_WRITE_DATA  = 1,    // 0x01
+    OT_MSG_INVALID     = 2,
+    OT_MSG_RESERVED    = 3,
+    OT_MSG_READ_ACK    = 4,
+    OT_MSG_WRITE_ACK   = 5,
+    OT_MSG_DATA_INVALID= 6,
+    OT_MSG_UNKNOWN_ID  = 7
+};
+
+// Message Data IDs
 enum OpenThermMessageID : uint8_t {
     OT_MSG_STATUS          = 0,    // 0x00 - Master/Slave Status flags
     OT_MSG_T_SET           = 1,    // 0x01 - Control Setpoint (Fault flags in read-ack)
     OT_MSG_FAULT_FLAGS     = 1,    // 0x01 - Application Specific Fault Flags
     OT_MSG_OEM_CODE        = 2,    // 0x02 - OEM Code
+    
     OT_MSG_REL_MOD_LEVEL   = 17,   // 0x11 - Relative Modulation Level
     OT_MSG_CH_WATER_PRESSURE = 18, // 0x12 - Water pressure
     OT_MSG_CH_WATER_TEMP   = 25,   // 0x19 - Boiler Flow Temperature
@@ -14,8 +28,6 @@ enum OpenThermMessageID : uint8_t {
     OT_MSG_RETURN_WATER_TEMP = 28, // 0x1C - Return Water Temperature
     
     // Specifieke / Afwijkende mappings uit jouw project
-    // Jouw oude code gebruikte 0x18 (24) voor Boiler Temp en 0x19 (25) voor Return. 
-    // Standaard OT is 25=Flow, 28=Return. Ik definieer ze hier zodat de namen kloppen met jouw ketel.
     OT_MSG_BOILER_TEMP_LEGACY = 24, // 0x18
     OT_MSG_RETURN_TEMP_LEGACY = 25, // 0x19
     
